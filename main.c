a4248d214c3322a8115fe6ba19d2956b7179b5ba
ashleyrene <ashleyrashidian@gmail.com>
2025-04-13 15:29:59 -0400

    Update main.c

--- main.c
+++ main.c
@@ -3,4 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) { return EXIT_SUCCESS; }
+int main(int argc, char **argv) {
+    assert(argc == 3);  // ensure correct number of arguments
+
+    Tree *root = read_from_file(argv[1]);
+    assert(root != NULL);  // ensure tree was read successfully
+
+    Tree *inverted = invert_tree(root);
+    assert(inverted != NULL);  // should not be NULL if root wasn't
+
+    preorder_print(inverted, argv[2]);
+
+    free_tree(inverted);    // free memory
+    return EXIT_SUCCESS;
+}
